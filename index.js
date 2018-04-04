9fbeba66cc44c8101b1f737e71be2fb1d0ee557c
Axel Mousset <pymzorr@gmail.com>
2018-04-04 10:33:32 +0200

    refact: move from var to const, reindent to 4 lines

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-'use strict';
+const debug            = require('debug')('express-idempotency');
+const connect          = require('connect');
+const expressEnd       = require('express-end');
 
-var debug = require('debug')('express-idempotency');
-var connect = require('connect');
-var expressEnd = require('express-end');
-
-var cache = require('./lib/cache-provider');
-var generateCacheKey = require('./lib/generate-cache-key');
+const cache            = require('./lib/cache-provider');
+const generateCacheKey = require('./lib/generate-cache-key');
 
 /**
  * Express middleware
@@ -13,58 +11,61 @@ var generateCacheKey = require('./lib/generate-cache-key');
  * @param {*} res 
  * @param {*} next 
  */
-var checkMw = function(req, res, next) {
-  var idempotencyKey = req.get('Idempotency-Key');
+const checkMw = (req, res, next) => {
+    const idempotencyKey = req.get('Idempotency-Key');
 
-  if (!idempotencyKey) {
-    return next();
-  }
+    if (!idempotencyKey) {
+        return next();
+    }
 
-  const cacheKey = generateCacheKey(req, idempotencyKey);
-  const storedResponse = cache.get(cacheKey);
+    const cacheKey       = generateCacheKey(req, idempotencyKey);
+    const storedResponse = cache.get(cacheKey);
 
-  if (!storedResponse) {
-    return next();
-  }
+    if (!storedResponse) {
+        return next();
+    }
 
-  res.status(storedResponse.statusCode);
-  res.set(storedResponse.headers);
-  res.set('X-Cache', 'HIT'); // indicate this was served from cache
-  res.send(storedResponse.body);
+    res.status(storedResponse.statusCode);
+    res.set(storedResponse.headers);
+    res.set('X-Cache', 'HIT'); // indicate this was served from cache
+    res.send(storedResponse.body);
 }
 
 /**
- * Express middleware to store a resposne against a supplied idempotency token
+ * Express middleware to store a response against a supplied idempotency token
  * in the cache.
  * @param {object} req Express request
  * @param {object} res Express response
  * @param {function} next Express next callback function
  */
-function storeMw(req, res, next) {
-  res.once('end', () => {
-    const idempotencyKey = req.get('Idempotency-Key');
-    if (idempotencyKey) {
-      const responseToStore = {
-        statusCode: res.statusCode,
-        body: res.body,
-        headers: res.headers,
-      };
+const storeMw = (req, res, next) => {
+    res.once('end', () => {
+        const idempotencyKey = req.get('Idempotency-Key');
+        
+        if (idempotencyKey) {
+            const responseToStore = {
+                statusCode: res.statusCode,
+                body: res.body,
+                headers: res.headers,
+            };
 
-      const cacheKey = generateCacheKey(req, idempotencyKey);
-      cache.set(cacheKey, responseToStore)
-      debug('stored response against idempotency key: ', idempotencyKey);
-    }
-  });
-  return next();
+            const cacheKey = generateCacheKey(req, idempotencyKey);
+            cache.set(cacheKey, responseToStore)
+            debug('stored response against idempotency key: ', idempotencyKey);
+        }
+    });
+
+    return next();
 }
 
-var idempotency = function (options) {
-  // chain pattern from helmet - see https://github.com/helmetjs/helmet/blob/master/index.js
-  var chain = connect();
-  chain.use(expressEnd);
-  chain.use(checkMw);
-  chain.use(storeMw);
-  return chain;
+const idempotency = (options) => {
+    // chain pattern from helmet - see https://github.com/helmetjs/helmet/blob/master/index.js
+    const chain = connect();
+    chain.use(expressEnd);
+    chain.use(checkMw);
+    chain.use(storeMw);
+
+    return chain;
 }
 
 module.exports = idempotency;
